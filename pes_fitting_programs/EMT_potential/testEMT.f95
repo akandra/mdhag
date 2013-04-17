fb6e4fa3959a97d8752cef990bbbc59eda4d8e2d
akandra <akandra1@gmail.com>
2013-04-17 15:37:54 +0200

    We started with emt.

--- pes_fitting_programs/EMT_potential/testEMT.f95
+++ pes_fitting_programs/EMT_potential/testEMT.f95
@@ -6,40 +6,31 @@ program testAtomClass
 
     type (atom)              :: particle
     type (atom), allocatable, dimension(:) :: lattice
+    real(8), allocatable, dimension(:,:) :: r0
     ! Data directory: declare variables and types
     type (EMTparms) :: particle_pars, lattice_pars
     namelist / lattice_pars_list /  lattice_pars
     namelist / particle_pars_list / particle_pars
 
+    real(8) :: energy
+    integer :: n_lat = 100            ! number of lattice atoms
+
     ! Read in the lattice parameters
     open(8,file='parameters_Au_f119.nml')
     read(8,nml=lattice_pars_list)
-    write(*,*) lattice_pars
     close(8)
     ! Read in the particle parameters
     open(8,file='parameters_H_f119.nml')
     read(8,nml=particle_pars_list)
-    write(*,*) particle_pars
     close(8)
 
+    ! allocate dynamic arrays
+    allocate(lattice(n_lat), r0(3,n_lat))
 
+    call emt (particle, lattice, r0, particle_pars, lattice_pars, energy)
 
-!    print *, "H positions with using defaults ", H%r
-!    print *, "H velocities with using defaults ", H%v
-!    print *, "H complete with using defaults ", H
-!    H = atom(0)
-!    print *, "H positions with using atom(0)  ", H%r
-!    D = H
-!    print *, "D positions with using D = H    ", H%r
+    print *, energy
 
-
-! allocate the slab
-!allocate(slab(100))
-
-!slab(7)%r=(/0.0, 0.0, 6.0/)
-!print *, 'This is he first atom coordinate', slab(1)%r
-!print *, 'this is the second:', slab(7)%r
-
-!deallocate(slab) ! not necessary but more beautiful
+    deallocate(lattice, r0)
 
 end program
