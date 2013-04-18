6e7e84a44ee746d437b7f6da5b61fb9e248fde0d
smjanke <sjanke@gwdg.de>
2013-04-18 18:00:54 +0200

    First trials with the implementation of EMT; still needs loads of work

--- pes_fitting_programs/EMT_potential/EMTparms_class.f95
+++ pes_fitting_programs/EMT_potential/EMTparms_class.f95
@@ -19,26 +19,83 @@ use atom_class
 
 contains
 
-subroutine emt_init (r0, pars_p, pars_l)
+subroutine emt_init (n_Au, r0_lat, r0_part, pars_p, pars_l, energy)
 !
 ! Purpose:
-!       emt calculates the energy according to the effective medium theory.
-! Input variables are (in oder of appearance):
-!           particle: incident particle
-!           lattice : lattice atoms
-!           r0      : equilibrium positions of lattice atoms
-!           pars_p  : emt-parameters of particle
-!           pars_l  : emt-parameters of lattice
-! Output variables are:
-!           energy  : emt-energy
+!           Here, the fitting procedure is just implemented and the reference energy calculated.
+!   Input variables:
+!                   r0_lat : equilibrium positions of the lattice atoms
+!                   r0_part: reference position of the particle (at 0,0, 6.0 A above the surface)
+!                   pars_p : parameters for the particle
+!                   pars_l : parameters for the lattice
+!
 implicit none
 
-    type(atom), intent (in)  :: particle, lattice(:)
-    real(8), dimension(:,:), intent (in) :: r0
-    type(EMTparms),   intent (in)  :: pars_p, pars_l
-    real(8),                 intent (out) :: energy
+! declare variables and parameters
+!    type(atom), intent (in)  :: particle, lattice(:)
+!    real(8), dimension(:,:), intent (in) :: r0
+!    type(EMTparms),   intent (in)  :: pars_p, pars_l
+    integer, intent(in)                 :: n_Au
+    real(8), dimension(3), intent (in)  :: r0_part
+    real(8), dimension(:,:), intent(in) :: r0_lat
+    type(EMTparms), intent(inout)       :: pars_p
+    type(EMTparms), intent(inout)       :: pars_l
+    real(8), intent(out)                :: energy
 
-    energy = 1347
+! declare the variables that appear in the program
+    real(8), parameter :: beta = 1.8093997906
+    real(8), parameter :: bohr = 0.529177
+
+
+    integer :: i,j                           ! running parameter
+    real(8), dimension(n_Au) :: r_HAu        ! distance between H and Au
+    real(8), dimension(n_Au,1) :: r_AuAu       ! distance between Au and Au
+    real(8) :: rcut, rr, acut                ! values to calculate cut-off
+    real(8), dimension(n_Au,1) :: xAu              ! help-array to calculate theta
+    real(8), dimension(n_Au) :: xH
+    real(8), dimension(n_Au,1) :: thetaAu    ! cut-off theta
+    real(8), dimension(n_Au) :: thetaH       ! cut-off theta
+    real(8), dimension(n_Au,1,3) :: xiAu     ! Array for gamma-calculation for Au
+    real(8), dimension(n_Au,3) :: xiH        ! Array for gamma-calculation for H
+
+    real(8),dimension(n_Au,1) :: tempAu=0    ! dummy array for Gold
+    real(8), dimension(n_Au) :: tempH=0      ! dummy array for hydrogen
+
+
+! calculate cut-off
+    rcut = pars_l%s0 * beta * sqrt(3.)
+    rr = 4. * rcut / (sqrt(3.) + 2.)
+    acut = log(9999.)/(rr -rcut)
+
+    ! calculate r_HAu and r_AuAu
+    rHauloop: do i = 1, n_Au
+
+            r_HAu(i) = sqrt((r0_lat(1,i)+r0_part(1))**2+(r0_lat(2,i)+r0_part(2))**2+(r0_lat(3,i)+r0_part(3))**2)
+
+    end do rHauloop
+
+! Start here with first Au-loop. Which views everything from the perspective of the 45th gold atom since that one is pretty much in the middle of the
+!   first layer and it's gonna be replaced by a j, anyway (or, by another layer-implementation).
+    rAuAuloop1: do i = 1, n_Au
+                    r_AuAu(i,1) = sqrt((r0_lat(1,i)-r0_lat(1,45))**2+(r0_lat(2,i)-r0_lat(2,45))**2+(r0_lat(3,i)-r0_lat(3,45))**2)
+
+
+! calculate theta
+                    xAu(i,1) =exp( acut * (r_AuAu(i,1) - rcut) )
+                    thetaAu(i,1) = 1 / (1 + xAu(i,1))
+
+                    xH(i) = exp( acut * (r_HAu(i) - rcut) )
+                    thetaH(i) = 1 / (1 + xH(i))
+
+! Calculate Gamma (this is not finished and will turn out to be rather vexing)
+                    rAunn = pars%s0 * beta / 1
+                    xiAu(i,1,1) = 1 / ( 1 + exp( acut (rAunn) ) )
+                    tempAu=
+                    write(*,*) thetaAu, thetaH
+
+
+    end do rAuAuloop1
+
 
 end subroutine emt_init
 
--- pes_fitting_programs/EMT_potential/testEMT.f95
+++ pes_fitting_programs/EMT_potential/testEMT.f95
@@ -3,24 +3,29 @@ program testAtomClass
     use atom_class
     use EMTparms_class
     implicit none
-
+    ! Data directory: declare variables and types
     type (atom)                          :: particle
     type (atom), allocatable,dimension(:):: lattice
     real(8), allocatable, dimension(:,:) :: r0_lat
     real(8), dimension(3)                :: r0_part
-    character, dimension(100)            :: reference_configuration_fname
+    character(len=30)                      :: reference_configuration_fname
 
-    reference_configuration_fname='ref_conf_Au111.dat'
 
-    ! Data directory: declare variables and types
     type (EMTparms) :: particle_pars, lattice_pars
     namelist / lattice_pars_list /  lattice_pars
     namelist / particle_pars_list / particle_pars
 
     real(8) :: energy
     integer :: n_lat = 2            ! number of lattice atoms
+    integer :: ierror               ! number to check if file opened correctly
+    integer :: n_lat0_at            ! number of atoms in reference slab
+    integer :: n_lay0               ! number of layers in reference slab
+    integer :: i                    ! running integer to read in reference lattice
+    real(8) :: nn0                  ! next neighbour distance in reference slab
+    real(8) :: a0                   ! lattice constant
 
 
+    reference_configuration_fname='ref_conf_Au111a.dat'
 
     ! Read in the lattice parameters
     open(8,file='parameters_Au_f119.nml')
@@ -33,18 +38,33 @@ program testAtomClass
     close(8)
 
     ! Read in the number of atoms in the lattice
-    open(8,file=reference_configuration_fname)
+    open(8, file=reference_configuration_fname, status='old', action='read', iostat=ierror)
     ! add spec that file exists and is read only.  error checking
-    read(8, *)
+        openif: if ( ierror == 0 ) then
+                    read(8, *) n_lat0_at
+                    read(8,*) n_lay0
+                    read(8,*) nn0
+                    ! read in the values for r0_lat
+                    allocate(r0_lat(3,n_lat0_at))
+!                    allocate(r0_lat(3,2))
+                    readr0lat: do i = 1, n_lat0_at
+                                read(8,*) r0_lat(1,i), r0_lat(2,i), r0_lat(3,i)
 
-    read
+                    end do readr0lat
+                    r0_part = (/0.0, 0.0, 6.0/)
+!                    print *, r0_lat
+!                    print *, n_lat0_at, n_lay0, nn0, r0_part
+                    call emt_init(n_lat0_at,r0_lat, r0_part, particle_pars, lattice_pars, energy)
+                    print *, energy
+
+    end if openif
 
     ! allocate dynamic arrays
     allocate(lattice(n_lat))
 
-!    call emt (particle, lattice, r0, particle_pars, lattice_pars, energy)
+ !   call emt_init (particle, lattice, r0, particle_pars, lattice_pars, energy)
 
-    print *, lattice
+!    print *, lattice
     deallocate(lattice)
 
 end program
