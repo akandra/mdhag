367ee18727408616260836613ab54d62cb2976bd
akandra <akandra1@gmail.com>
2013-04-23 11:16:16 +0200

    finishes emt reference input

--- pes_fitting_programs/EMT_potential/EMTparms_class.f95
+++ pes_fitting_programs/EMT_potential/EMTparms_class.f95
@@ -7,6 +7,7 @@ use atom_class
     real(8), parameter :: isqrt_2 = 0.70710678118
     real(8), parameter :: pi = 3.14159265359
     real(8), parameter :: beta = 1.8093997906
+    real(8), parameter :: twelveth = 0.0833333333333333
     integer, dimension(3), parameter :: b = (/12, 6, 24/)
 
     type EMTparms
@@ -54,12 +55,12 @@ implicit none
     integer :: i,j, k                        ! running parameter
     real(8) :: r                             ! distance
     real(8) :: rcut, rr, acut                ! values to calculate cut-off
-    real(8) :: gamma1H, gamma2H, Ecoh
-    real(8) :: theta, gamma1Au, gamma2Au, chiAuH
-    real(8) :: sigma_HAu, s_H
+    real(8) :: igamma1H, igamma2H, Ecoh
+    real(8) :: theta, igamma1Au, igamma2Au, chiAuH, chiHAu
+    real(8) :: sigma_HAu, s_H, V_HAu, V_AuH, V_AuAu, vref_Au, vref_H
     real(8), dimension(n_Au) :: sigma_AuAu, sigma_AuH, s_Au
     real(8), dimension(3) :: xAu, xH, rnnAu, rnnH
-    real(8) :: rtemp, betas0_l, betas0_p     ! temporary real variables
+    real(8) :: rtemp, betas0_l, betas0_p, kappadbeta_l, kappadbeta_p     ! temporary real variables
     real(8), dimension(3) :: r3temp
 
 ! calculate cut-off
@@ -67,8 +68,11 @@ implicit none
 
     betas0_l = beta * pars_l%s0
     betas0_p = beta * pars_p%s0
+    kappadbeta_l = pars_l%kappa / beta
+    kappadbeta_p = pars_p%kappa / beta
 
     chiAuH = pars_p%n0 / pars_l%n0
+    chiHAu = 1.0 / chiAuH
 
     rcut = betas0_l * sqrt_3
     rr = 4 * rcut / (sqrt_3 + 2)
@@ -81,23 +85,26 @@ implicit none
     rnnH(2) = rnnH(1) * sqrt_2
     rnnH(3) = rnnH(1) * sqrt_3
 
-    xAu = b / (12 * (1 + exp(acut*(rnnAu-rcut))))
-    xH = b / (12 * (1 + exp(acut*(rnnH-rcut))))
+    xAu = b * twelveth / (1 + exp(acut*(rnnAu-rcut)))
+    xH = b * twelveth/ (1 + exp(acut*(rnnH-rcut)))
 
  ! Definition of gamma
     r3temp = rnnAu-betas0_l
-    gamma1Au = sum(xAu*exp(-pars_l%eta2 * r3temp))
-    gamma2Au = sum(xAu*exp(-pars_l%kappa/beta * r3temp))
+    igamma1Au = 1.0 / sum(xAu*exp(-pars_l%eta2 * r3temp))
+    igamma2Au = 1.0 /sum(xAu*exp(-kappadbeta_l * r3temp))
 
     r3temp = rnnH-betas0_p
-    gamma1H = sum(xH*exp(-pars_p%eta2 * r3temp))
-    gamma2H = sum(xH*exp(-pars_p%kappa/beta * r3temp))
+    igamma1H = 1.0 / sum(xH*exp(-pars_p%eta2 * r3temp))
+    igamma2H = 1.0 / sum(xH*exp(-kappadbeta_p * r3temp))
 
 
 ! Here, the main loop starts.
 
     sigma_AuAu = 0
     sigma_HAu = 0
+    V_AuAu = 0
+    V_AuH = 0
+    V_HAu = 0
 
     do i = 1, n_Au
     ! calculate r_HAu and r_AuAu. Now we have periodic boundery conditions, too. Muhahahaha!
@@ -112,12 +119,18 @@ implicit none
             r =  sqrt(sum(r3temp**2))
 
             ! calculate theta for lattice
-            theta = 1 / (1 + exp( acut * (r - rcut) ) )
+            theta = 1.0 / (1 + exp( acut * (r - rcut) ) )
 
             ! calculate sigma
             rtemp = theta*exp(-pars_l%eta2 * (r - betas0_l) )
             sigma_AuAu(i) = sigma_AuAu(i) + rtemp
             sigma_AuAu(j) = sigma_AuAu(j) + rtemp
+
+            ! calculate V
+            rtemp = theta*exp(-kappadbeta_l * (r - betas0_l))
+            V_AuAu = V_AuAu + rtemp
+
+
         end do
 
         r3temp(1) = r0_lat(1,i)-r0_part(1)
@@ -128,31 +141,45 @@ implicit none
         r =  sqrt(sum(r3temp**2))
 
         ! calculate theta
-        theta = 1 / (1 + exp( acut * (r - rcut) ) )
+        theta = 1.0 / (1 + exp( acut * (r - rcut) ) )
 
         ! calculate sigma
         sigma_AuH(i) = theta*exp(-pars_p%eta2 * (r - betas0_p) )
         rtemp = theta*exp(-pars_l%eta2 * (r - betas0_l) )
         sigma_HAu = sigma_HAu + rtemp
 
+        ! calculate mixed V
+        rtemp = theta*exp(-kappadbeta_p * (r - betas0_p))
+        V_AuH = V_AuH + rtemp
+        rtemp = theta*exp(-kappadbeta_l * (r - betas0_l))
+        V_HAu = V_HAu + rtemp
+
     end do
 
     ! don't forget the gamma
-    sigma_AuAu = sigma_AuAu / gamma1Au
-    sigma_AuH = sigma_AuH / gamma1Au
-    sigma_HAu = sigma_HAu / gamma1H
+    sigma_AuAu = sigma_AuAu * igamma1Au
+    sigma_AuH = sigma_AuH * igamma1Au
+    sigma_HAu = sigma_HAu * igamma1H
+
+    V_AuAu = V_AuAu * pars_l%V0 * igamma2Au
+    V_AuH = V_AuH *chiAuH * pars_l%V0 * igamma2Au
+    V_HAu = V_HAu * pars_p%V0 * igamma2H * chiHAu
+
 
     ! calculation of s
-    s_Au = -log( (sigma_AuAu + chiAuH * sigma_AuH) / 12 ) / ( beta * pars_l%eta2)
-    s_H  = -log( sigma_HAu / (12*chiAuH) ) / ( beta * pars_p%eta2)
+    s_Au = -log( (sigma_AuAu + chiAuH * sigma_AuH) * twelveth ) / ( beta * pars_l%eta2)
+    s_H  = -log( sigma_HAu * chiHAu * twelveth) / ( beta * pars_p%eta2)
+
+    ! calculate reference V
+    vref_Au = 12 * pars_l%V0 * sum( exp( -pars_l%kappa * s_Au) )
+    vref_H = 12 * pars_p%V0 * exp( -pars_p%kappa * s_H)
 
     ! calculation of cohesive function
     Ecoh = sum( (1 + pars_l%lambda*s_Au) * exp(-pars_l%lambda * s_Au)-1 ) * pars_l%E0&
             + (1 + pars_p%lambda*s_H) * exp(-pars_p%lambda * s_H)* pars_p%E0
 
-
-    print *, Ecoh
-
+    ! Glorious sum
+    energy = Ecoh - V_AuAu - 0.5 * ( V_AuH + V_HAu - vref_Au - vref_H)
 
 end subroutine emt_init
 
