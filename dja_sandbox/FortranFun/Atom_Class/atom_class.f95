1a15cc5a7f256d6a78a6195ab048add54b00a27f
Daniel Auerbach <daniel@djauerbach.com>
2013-04-28 12:49:27 +0200

    Updated the demo_atom_class program this was formerly called testAtomClass added comments, prettified printout, added printout of locations of components of structures

--- dja_sandbox/FortranFun/Atom_Class/atom_class.f95
+++ dja_sandbox/FortranFun/Atom_Class/atom_class.f95
@@ -1,52 +1,68 @@
 module atom_class
     implicit none
-    public                              ! sorry, for performance in accessing components
+    public         ! public for performance in accessing components
 
+    ! -------------------------------------------------------------------------
+    !                  Type atom                                              |
+    ! -------------------------------------------------------------------------
+    !  structure to hold the position, velocity, and force on a single atom   |
+    ! -------------------------------------------------------------------------
     type atom
-        real, dimension(3)      :: r=1  ! position
-        real, dimension(3)      :: v=2  ! velocity
-        real, dimension(3)      :: f=3  ! force
+        real(8), dimension(3)      :: r=(/1,2,3/)  ! position
+        real(8), dimension(3)      :: v=(/4,5,6/)  ! velocity
+        real(8), dimension(3)      :: f=(/7,8,9/)  ! force
     end type atom
 
+    !-------------------------------------------------------------------------|
+    !                  Type atoms                                             |
+    !-------------------------------------------------------------------------|
+    ! structure to hold the position, velocity, and force on a multiple atoms |
+    !    use rank2 array so positions, velocities, and forces are stored      |
+    !    in sequentional memory locations for efficient access                |
+    !    each array should be allocated (3,n_atom)                            |
+    !-------------------------------------------------------------------------|
+
     type atoms
-        integer                             :: n_atoms=1!number of atoms
-        real, dimension(:,:), allocatable   :: r        ! positions (3,natoms
-        real, dimension(:,:), allocatable   :: v        ! velocities
-        real, dimension(:,:), allocatable   :: f        ! forces
+        integer                                :: n_atoms  ! number of atoms
+        real(8), dimension(:,:), allocatable   :: r        ! positions
+        real(8), dimension(:,:), allocatable   :: v        ! velocities
+        real(8), dimension(:,:), allocatable   :: f        ! forces
     end type atoms
 
 
+    !-------------------------------------------------------------------------|
+    !                  Constructor for type atoms                             |
+    !-------------------------------------------------------------------------|
+    !    input:  n_atoms                                                      |
+    !    allocates arrays as (3,n_atom)                                       |
+    !                                                                         |
+    !                                                                         |
+    !-------------------------------------------------------------------------|
 
 
-    type atoms2
-        integer                             :: n_atoms=1!number of atoms
-        real, dimension(:,:), allocatable   :: r        ! positions (3,natoms
-        real, dimension(:,:), allocatable   :: v        ! velocities
-        real, dimension(:,:), allocatable   :: f        ! forces
-    end type atoms2
 
-    interface atoms2
-        module procedure new_atoms2
+    interface atoms
+        module procedure new_atoms
     end interface
 
 contains
 
-    function new_atoms2(i)
-        integer, intent(in) :: i
-        type(atoms2) new_atoms2
+    function new_atoms(n_atoms)
+        integer, intent(in) :: n_atoms
+        type(atoms) new_atoms
 !-------------------------------------------------
 !   allocate r, v, and f
 !-------------------------------------------------
-        allocate(new_atoms2%r(3,i))
-        allocate(new_atoms2%v(3,i))
-        allocate(new_atoms2%f(3,i))
+        allocate(new_atoms%r(3,n_atoms))
+        allocate(new_atoms%v(3,n_atoms))
+        allocate(new_atoms%f(3,n_atoms))
 
 !-------------------------------------------------
 !   initialize r, v, and f
 !-------------------------------------------------
-        new_atoms2%r = 1
-        new_atoms2%v = 2
-        new_atoms2%f = 3
+        new_atoms%r = 0
+        new_atoms%v = 0
+        new_atoms%f = 0
     end function
 
 
--- dja_sandbox/FortranFun/Atom_Class/demo_atom_class.f95
+++ dja_sandbox/FortranFun/Atom_Class/demo_atom_class.f95
@@ -3,30 +3,17 @@ program testAtomClass
     use atom_class
     implicit none
 
-
-
-    type (atom)  ,target            :: particle
-    type (atoms) ,target            :: lat1
-    type (atoms) ,target            :: lat2
-
-    integer, parameter              :: n_atoms=4
+    type (atom)  ,target            :: particle     ! used to demonstrate atom object
+    type (atoms) ,target            :: lat1         ! used to demonstrate manual allocation of atoms object components
+    type (atoms) ,target            :: lat2         ! used to demonstrate use of construct for
+    ! alloction of atoms object components
+    integer, parameter              :: n_atoms=4    ! number of atoms for the atoms objects
     integer                         :: i,j
 
-    real(8), dimension(3,n_atoms)   :: test1
-    real(8), dimension(3,n_atoms)   :: test2
-    real(8), dimension(n_atoms)     :: test3
-    real(8), dimension(n_atoms)     :: test4
-
-    real(8), pointer, dimension(:)     :: patom
-    real(8), pointer, dimension(:,:)   :: patoms
-
     namelist / particle_list /  particle
-    !namelist / lattice_list1 /  lat1   not allowed becuase has allocatable
-    !namelist / lattice_list2 /  lat2   not allowed becuase has allocatable
+    !namelist / lattice_list1 /  lat1   not allowed becuase has allocatable elements.
 
-
-    print *, 'particle r       v       f'
-    print '(6x,I5,3F8.2,(6X,3f8.2))', particle
+    print '((a),9f5.1)', 'particle=', particle
 
     print *
     print *,'output using namelist'
@@ -63,17 +50,19 @@ program testAtomClass
     print *, '--------------------------------------------------------------------------'
     print *, '                              lattice 1                                   '
     print *, '--------------------------------------------------------------------------'
-    print *, ''
+    print *, 'atom      position            velocity            force'
+    print *, '--------------------------------------------------------------------------'
     do j=1,n_atoms
-        print 1000, j, "r=", (lat1%r(i,j),i=1,3), "v=", (lat1%v(i,j),i=1,3), "f=", (lat1%v(i,j),i=1,3)
-    enddo
-
+        print 1000, j,((lat1%r(i,j)),i=1,3), ((lat1%v(i,j)),i=1,3), ((lat1%f(i,j)),i=1,3)
+    end do
     print *, ''
     print *, '--------------------------------------------------------------------------'
     print *, '                              lattice 2                                   '
     print *, '--------------------------------------------------------------------------'
+    print *, 'atom      position            velocity            force'
+    print *, '--------------------------------------------------------------------------'
     do j=1,n_atoms
-        print 1000, j, "r=", (lat2%r(i,j),i=1,3), "v=", (lat2%v(i,j),i=1,3), "f=", (lat2%f(i,j),i=1,3)
+        print 1000, j, (lat2%r(i,j),i=1,3), (lat2%v(i,j),i=1,3), (lat2%f(i,j),i=1,3)
     enddo
 
     !-------------------------------------------------------------------------!
@@ -85,7 +74,6 @@ program testAtomClass
     print *, '--------------------------------------------------------------------------'
     print *, '                        location of variables in particle                 '
     print *, '--------------------------------------------------------------------------'
-    !rint 1100, 'n_atoms : ', loc(particle%n_atoms)
     print 1100, 'position: ', ((loc(particle%r(i))),i=1,3)
     print 1100, 'velocity: ', ((loc(particle%v(i))),i=1,3)
     print 1100, 'force   : ', ((loc(particle%f(i))),i=1,3)
@@ -111,29 +99,8 @@ program testAtomClass
     print 1100, 'force   : ', (((loc(lat2%f(i,j))),i=1,3), j=1,n_atoms)
     print *
 
-    print *, ''
-    print *, '--------------------------------------------------------------------------'
-    print *, '             location of variables in declare arrays test1, test2         '
-    print *, '--------------------------------------------------------------------------'
-    print 1100, 'test1   : ', (((loc(test1(i,j))),i=1,3), j=1,n_atoms)
-    print 1100, 'test2:    ', (((loc(test2(i,j))),i=1,3), j=1,n_atoms)
-    print *
 
-    print *, ''
-    print *, '--------------------------------------------------------------------------'
-    print *, '             location of variables in declare arrays test3, test4         '
-    print *, '--------------------------------------------------------------------------'
-    print 1100, 'test3   : ', ((loc(test3(i))),i=1,3)
-    print 1100, 'test4:    ', ((loc(test4(i))),i=1,3)
-    print *
-    print *, 'size of test1=',size(test1)
-
-
-    write(*,'(//,"use pointers to access positions")')
-    patom  => particle%r
-    write(*,2000) (patom(i),i=1,9)
-
-1000 format (i5, a5, 3f5.1, 2(5x, a5, 3f5.1) )
-1100 format ((a)/(3i9,2x))
-2000 format (9f5.0)
+    1000 format (i5, 5x, 3f5.1, 2(5x, 3f5.1) )
+    1100 format ((a)/(3i9,2x))
+    2000 format (9f5.0)
 end program
